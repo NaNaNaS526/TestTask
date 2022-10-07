fd6a0a46fb033eee80ff6e1cfbc1ddaae8093d35
NaNaNaS <tairvildanov7@gmail.com>
2022-10-07 18:38:51 +0500

    I made a working menu, background music and volume saving when exiting

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Button soundSettingsButton;
+    [SerializeField] private GameObject soundSettingsPanel;
+    [SerializeField] private TextMeshProUGUI soundVolumeText;
+    [SerializeField] private Slider soundVolumeSlider;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (PlayerPrefs.GetInt("SoundVolume") != 0)
+        {
+            int soundVolume = PlayerPrefs.GetInt("SoundVolume");
+            soundVolumeSlider.value = soundVolume;
+        }
+        soundSettingsButton.onClick.AddListener(DisplaySoundSettingsPanel);
+    }
+
+    private void Update()
+    {
+        _audioSource.volume = soundVolumeSlider.value / 100;
+        var soundVolume = (int)soundVolumeSlider.value;
+        soundVolumeText.text = $"Sound: {soundVolume.ToString()}";
+    }
+
+    private void DisplaySoundSettingsPanel()
+    {
+        soundSettingsPanel.SetActive(soundSettingsPanel.activeSelf == false);
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("SoundVolume", (int)soundVolumeSlider.value);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.SceneManagement.SceneManager;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private GameObject audioManager;
+    [SerializeField] private GameObject canvas;
+
+    private void Start()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+        IndestructibleObjectsSettings();
+    }
+
+    private void IndestructibleObjectsSettings()
+    {
+        DontDestroyOnLoad(canvas);
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(audioManager);
+    }
+    private void StartGame()
+    {
+        LoadScene(1);
+        startGameButton.gameObject.SetActive(false);
+    }
+
+    
+}
